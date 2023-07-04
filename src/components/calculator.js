14fdbed7f1b05a8a7a6d83e5b3528e01798f7b0f
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-04 18:32:48 +0500

    Exporting files individually

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,42 +1,7 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import PropTypes from 'prop-types';
 import './calculator.css';
-
-function RowCalculator(props) {
-  const { items } = props;
-  return (
-    <div className="row">
-      { items.map((item, index) => (
-        <div key={item} className={`col-3 button text-center py-3 border ${index % 4 === 3 ? 'last-item' : ''}`}>{item}</div>
-      ))}
-    </div>
-  );
-}
-RowCalculator.propTypes = { items: PropTypes.arrayOf(PropTypes.string).isRequired };
-
-function ResultScreen(props) {
-  const { answere } = props;
-  return (
-    <div className="row">
-      <div className="col-12 bg-secondary text-light py-3 text-end">{answere}</div>
-    </div>
-  );
-}
-ResultScreen.propTypes = { answere: PropTypes.number };
-ResultScreen.defaultProps = { answere: 0 };
-
-function EndRow(props) {
-  const { items } = props;
-  return (
-    <div className="row">
-      <div key={items[0]} className="col-6 button text-center py-3 border">{ items[0] }</div>
-      <div key={items[1]} className="col-3 button  text-center py-3 border">{ items[1] }</div>
-      <div key={items[2]} className="col-3 text-center py-3 border last-item">{ items[2] }</div>
-
-    </div>
-  );
-}
-EndRow.propTypes = { items: PropTypes.arrayOf(PropTypes.string).isRequired };
+import RowCalculator from './Calculator/RowCalculator';
+import ResultScreen from './Calculator/ResultScreen';
+import EndRow from './Calculator/EndRow';
 
 function Calculator() {
   return (
@@ -50,4 +15,5 @@ function Calculator() {
     </div>
   );
 }
+
 export default Calculator;
