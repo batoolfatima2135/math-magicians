e23dc78fc294a47dfb997f24dae9512fc756a5cb
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-05 00:27:39 +0500

    Add state and setstate

--- src/components/Calculator/EndRow.js
+++ src/components/Calculator/EndRow.js
@@ -1,17 +1,30 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
+import calculate from '../logic/calculate';
 
 function EndRow(props) {
-  const { items } = props;
+  const { items, state, updateState } = props;
+  const getdata = (event) => {
+    const btn = event.target.textContent;
+    updateState(calculate(state, btn));
+  };
   return (
     <div className="row">
-      <div key={items[0]} className="col-6 button text-center py-3 border">{ items[0] }</div>
-      <div key={items[1]} className="col-3 button  text-center py-3 border">{ items[1] }</div>
-      <div key={items[2]} className="col-3 text-center py-3 border last-item">{ items[2] }</div>
+      <button type="button" key={items[0]} onClick={getdata} className="col-6 button text-center py-3 border">{ items[0] }</button>
+      <button type="button" key={items[1]} onClick={getdata} className="col-3 button  text-center py-3 border">{ items[1] }</button>
+      <button type="button" key={items[2]} onClick={getdata} className="col-3 text-center py-3 border last-item">{ items[2] }</button>
 
     </div>
   );
 }
-EndRow.propTypes = { items: PropTypes.arrayOf(PropTypes.string).isRequired };
+EndRow.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+  state: PropTypes.shape({
+    total: PropTypes.string,
+    next: PropTypes.string,
+    operation: PropTypes.string,
+  }).isRequired,
+  updateState: PropTypes.func.isRequired,
+};
 
 export default EndRow;
--- src/components/Calculator/RowCalculator.js
+++ src/components/Calculator/RowCalculator.js
@@ -1,16 +1,29 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
+import calculate from '../logic/calculate';
 
 function RowCalculator(props) {
-  const { items } = props;
+  const { items, state, updateState } = props;
+  const getdata = (event) => {
+    const btn = event.target.textContent;
+    updateState(calculate(state, btn));
+  };
   return (
     <div className="row">
       { items.map((item, index) => (
-        <div key={item} className={`col-3 button text-center py-3 border ${index % 4 === 3 ? 'last-item' : ''}`}>{item}</div>
+        <button type="button" key={item} onClick={getdata} className={`col-3 button text-center py-3 border ${index % 4 === 3 ? 'last-item' : ''}`}>{item}</button>
       ))}
     </div>
   );
 }
-RowCalculator.propTypes = { items: PropTypes.arrayOf(PropTypes.string).isRequired };
+RowCalculator.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+  state: PropTypes.shape({
+    total: PropTypes.string,
+    next: PropTypes.string,
+    operation: PropTypes.string,
+  }).isRequired,
+  updateState: PropTypes.func.isRequired,
+};
 
 export default RowCalculator;
