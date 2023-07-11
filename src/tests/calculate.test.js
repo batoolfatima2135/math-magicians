af66e1ea274deab086481c1027d6c7f4099bcb6b
Batool Fatima <batoolfatima2135@gmail.com>
2023-07-11 19:02:24 +0500

    Add test for calculate.js file

---
+++ src/tests/calculate.test.js
@@ -0,0 +1,39 @@
+import calculate from '../components/logic/calculate';
+
+describe('calculate', () => {
+  test('returns updated object with AC button', () => {
+    const result = calculate({ total: '5', next: '3', operation: '+' }, 'AC');
+    expect(result).toEqual({
+      total: null,
+      next: null,
+      operation: null,
+    });
+  });
+
+  test('updates object with number button', () => {
+    const result = calculate({ total: '5', next: null, operation: '-' }, '2');
+    expect(result).toEqual({
+      total: '5',
+      next: '2',
+      operation: '-',
+    });
+  });
+
+  test('updates object with decimal button', () => {
+    const result = calculate({ total: '5', next: null, operation: null }, '.');
+    expect(result).toEqual({
+      total: '5',
+      next: '5.',
+      operation: null,
+    });
+  });
+
+  test('do the operation', () => {
+    const result = calculate({ total: '5', next: '4', operation: '+' }, '=');
+    expect(result).toEqual({
+      total: '9',
+      next: null,
+      operation: null,
+    });
+  });
+});
